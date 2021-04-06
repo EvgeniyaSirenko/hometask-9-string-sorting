a3dd94690e9d306a42c5fd46510668c5fc6c138d
sirenko.jane@gmail.com <Tyag1982>
2021-04-06 15:50:39 +0300

    init commit

---
+++ main/java/ua/mainacademy/AppRunner.java
@@ -0,0 +1,11 @@
+package ua.mainacademy;
+
+import java.util.logging.Logger;
+
+public class AppRunner {
+	public static final Logger LOG = Logger.getLogger(AppRunner.class.getName());
+	
+	public static void main(String[] args) {
+		LOG.info("Result is ");
+	}
+}
