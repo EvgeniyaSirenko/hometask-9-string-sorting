a1f6056d94c2e9d2c0afabd16f6caa1346360f56
sirenko.jane@gmail.com <Tyag1982>
2021-04-15 19:32:18 +0300

    sorting by quantity is done

--- main/java/ua/mainacademy/AppRunner.java
+++ main/java/ua/mainacademy/AppRunner.java
@@ -10,7 +10,7 @@ public class AppRunner {
 		Comparator<K> valueComparator = new Comparator<K>() {
 			public int compare(K k1, K k2) {
 				int compare =
-						map.get(k1).compareTo(map.get(k2));
+						map.get(k2).compareTo(map.get(k1));
 				if (compare == 0)
 					return 1;
 				else
@@ -18,7 +18,7 @@ public class AppRunner {
 			}
 		};
 		
-		Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
+		Map<K, V> sortedByValues = new TreeMap<>(valueComparator);
 		sortedByValues.putAll(map);
 		return sortedByValues;
 	}
